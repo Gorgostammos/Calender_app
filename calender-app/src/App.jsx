614c029ebae2789038ad9c6c8f16853eaf0a262f
Gorgos Tammo <gorgos2002@icloud.com>
2025-08-27 19:07:23 +0200

    Fix

---
+++ calender-app/src/App.jsx
@@ -0,0 +1,14 @@
+import CalenderApp from "./Components/CalenderApp"
+import'./Components/CalenderApp.css'
+
+
+const App = () => {
+  return (
+    <div className="container">
+      <CalenderApp/>
+
+    </div>
+  )
+}
+
+export default App
