fdadd1bcd1a62e8f4df99457734d687315f0fb46
Gorgos Tammo <gorgos2002@icloud.com>
2025-09-02 14:44:19 +0200

    h

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import CalenderApp from "./Components/CalenderApp"
 import'./Components/CalenderApp.css'
+import Login from "./login/login"
 
 
 const App = () => {
   return (
     <div className="container">
-      <CalenderApp/>
+      <Login/>
 
     </div>
   )
